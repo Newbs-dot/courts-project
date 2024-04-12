20e58ead7e8e985de25f0c13ea1eb19b5b5c1fe8
ma.maculevich <ma.maculevich@tensor.ru>
2024-04-12 15:28:43 +0500

    fix paths

--- project/courts_parser/main.py
+++ project/courts_parser/main.py
@@ -2,16 +2,23 @@ from pprint import pprint
 import json
 from court_parser import Parser
 from spacy_extractor import SpacyExtractor
+import os
+
 
 
 doc_name = './test_documents/A03-3155-2020_20200429_Reshenija_i_postanovlenija.pdf'
 doc_name2 = './train_documents/2 ответчика.pdf'
-doc_name3 = 'test_documents\A83-19868-2020_20201207_Opredelenie (1).pdf'
-parser = Parser('./spacy_models/output237/model-best','./spacy_models/output237_sums/model-last')
 
-print("***REGEX***")
-dop_info = parser.extract_info_regex(doc_name3)
-pprint(dop_info)
+doc_name3 = os.path.join(os.path.dirname(__file__), 'test_documents/A83-19868-2020_20201207_Opredelenie (1).pdf')
+
+main_model = os.path.join(os.path.dirname(__file__),'spacy_models/output237/model-best')
+sums_model = os.path.join(os.path.dirname(__file__),'spacy_models/output237_sums/model-last')
+
+parser = Parser(main_model,sums_model)
+
+# print("***REGEX***")
+# dop_info = parser.extract_info_regex(doc_name3)
+# pprint(dop_info)
 
 print("***SPACY***")
 
